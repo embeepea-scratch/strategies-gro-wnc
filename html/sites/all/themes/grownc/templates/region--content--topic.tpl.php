695d2722526430c08b815c140c4c76572839d529
Mark Phillips <embeepea@git>
2013-11-12 15:08:56 -0500

    initial nappl setup (imported drutils dump file var-strategies.gro-wnc.org-html--2013-11-12--15-18-57.tgz)

---
+++ html/sites/all/themes/grownc/templates/region--content--topic.tpl.php
@@ -0,0 +1,5 @@
+<div<?php print $attributes; ?>>
+  <?php if ($tabs && !empty($tabs['#primary'])): ?><div class="tabs clearfix"><?php print render($tabs); ?></div><?php endif; ?>
+  <?php if ($action_links): ?><ul class="action-links"><?php print render($action_links); ?></ul><?php endif; ?>
+  <?php print $content; ?>
+</div>
